71a4a03d8ac1ee016a5bd9bba400ee377cc0f03e
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-05-31 15:56:58 +0800

    feat: popup context

--- src/lib/Marker.svelte
+++ src/lib/Marker.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
-  import { type LatLngExpression, type MarkerOptions, Marker } from 'leaflet';
-  import { onMount, onDestroy, setContext, getContext } from 'svelte';
-  import { useConsumeMap } from './Map.svelte';
+	import { type LatLngExpression, type MarkerOptions, Marker } from 'leaflet';
+	import { onMount, onDestroy, setContext, getContext } from 'svelte';
+	import { useConsumeMap } from './Map.svelte';
+	import { useConsumePopup } from './Popup.svelte';
 
-  export let latlng: LatLngExpression;
-  export let options: MarkerOptions = {};
+	export let latlng: LatLngExpression;
+	export let options: MarkerOptions = {};
 
-  let map = useConsumeMap()(); 
+	let map = useConsumeMap()();
+	let popupStore = useConsumePopup()(); // TODO 处理组件不挂载在其中的错误
 
-  onMount(() => {
-    if (map) {
-      let marker = new Marker(latlng, options);
-      marker.addTo(map);
-    }
-    
-  })
+	let marker: Marker | undefined;
 
-  onDestroy(() => {})
+	onMount(() => {
+		if (map) {
+			marker = new Marker(latlng, options);
+			marker.addTo(map);
+		}
+	});
 
+	onDestroy(() => {});
+
+	$: if ($popupStore && marker) {
+		let popupContent = $popupStore.options.content || '';
+		marker.bindPopup(popupContent).openPopup();
+	}
+
+  $: console.log('zzh popup', $popupStore, marker, map);
 </script>
