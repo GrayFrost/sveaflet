8dd3ff8b4e46e1bb5717fbfaece155481a5780da
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-05-31 11:11:29 +0800

    feat: init map marker tilelayer

---
+++ src/lib/Map.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { Map, type MapOptions } from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+	import { useProvideMap } from './context.ts';
+
+	let map: Map;
+	let mapContainer: HTMLElement;
+
+	export let options: MapOptions = {};
+
+	useProvideMap(() => map);
+
+	onMount(() => {
+		map = new Map(mapContainer, options);
+		map.setView([51.505, -0.09], 13);
+
+		return () => {
+			map.remove();
+		};
+
+		// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		// 	attribution:
+		// 		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		// }).addTo(map);
+
+		// L.marker([51.5, -0.09])
+		// 	.addTo(map)
+		// 	.bindPopup('A pretty CSS popup.<br> Easily customizable.')
+		// 	.openPopup();
+	});
+</script>
+
+<div id="sveaflet-map" bind:this={mapContainer}>
+	{#if map}
+		<slot />
+	{/if}
+</div>
+
+<style>
+	#sveaflet-map {
+		width: 100vw;
+		height: 500px;
+	}
+</style>
---
+++ src/lib/types.ts
@@ -0,0 +1,4 @@
+import type { Map } from 'leaflet';
+export interface MapContext {
+  (): Map
+}
