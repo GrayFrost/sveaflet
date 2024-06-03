4c3ba8e81c4bceb2bedbec086fdab6a01b2e9e7e
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-03 17:53:14 +0800

    feat: tilelayer wms

---
+++ src/lib/TileLayerWMS.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { TileLayer, type WMSOptions } from 'leaflet';
+  import { useConsumeMap } from './context.ts';
+
+	export let baseUrl: string;
+	export let options: WMSOptions = {};
+	
+  let mapStore = useConsumeMap();
+
+	if ($mapStore) {
+		let tailLayerWMS = new TileLayer.WMS(baseUrl, options);
+		tailLayerWMS.addTo($mapStore);
+	}
+</script>
