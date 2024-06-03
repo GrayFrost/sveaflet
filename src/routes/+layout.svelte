71b73a636cfcea21d7a212374afbe734d44f9f4a
zzh <1760655799@qq.com>
2024-06-03 23:19:27 +0800

    feat: flowbite-svelte ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+	import { DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
 	import '../app.css';
+
+	let divClass = 'w-full ms-auto lg:block lg:w-auto order-1 lg:order-none';
+	let ulClass =
+		'flex flex-col py-3 my-4 lg:flex-row lg:my-0 text-sm font-medium text-gray-900 dark:text-gray-300 gap-4';
 </script>
 
 <header
 	class="sticky top-0 z-40 flex-none w-full mx-auto bg-white border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800"
 >
-	header
+	<Navbar>
+		<NavBrand href="/" />
+		<NavUl
+			{divClass}
+			{ulClass}
+			nonActiveClass="md:!ps-3 md:!py-2 lg:!ps-0 text-gray-700 hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 dark:text-gray-400 lg:dark:text-white lg:dark:hover:text-primary-700 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
+			activeClass="md:!ps-3 md:!py-2 lg:!ps-0 text-white bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:dark:text-primary-700 dark:bg-primary-600 lg:dark:bg-transparent cursor-default"
+		>
+			<NavLi class="lg:px-2 lg:mb-0" href="/">Home</NavLi>
+			<NavLi class="lg:px-2 lg:mb-0" href="/docs/pages/introduction">Docs</NavLi>
+			<NavLi class="lg:px-2 lg:mb-0" href="/docs/components/map">Components</NavLi>
+		</NavUl>
+	</Navbar>
 </header>
 <div class="lg:flex">
 	<slot />
