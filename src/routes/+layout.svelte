4ba74c45b6572258d2fe2ef838e9078270d06c5d
zzh <1760655799@qq.com>
2024-06-03 22:53:17 +0800

    feat: doc

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
-  import "../app.css";
+<script lang="ts">
+	import '../app.css';
 </script>
 
-<slot />
+<header
+	class="sticky top-0 z-40 flex-none w-full mx-auto bg-white border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800"
+>
+	header
+</header>
+<div class="lg:flex">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,75 +23,81 @@
 	};
 </script>
 
-<Map
-	on:click={onMapClick}
-	options={{
-		attributionControl: false
-	}}
+<main
+	class="flex-auto min-w-0 lg:static lg:max-h-full lg:overflow-visible divide-y dark:divide-gray-700"
 >
-	<!-- <TileLayer
+	<div class="w-[500px] h-[500px]">
+		<Map
+			on:click={onMapClick}
+			options={{
+				attributionControl: false
+			}}
+		>
+			<!-- <TileLayer
 		urlTemplate={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
 		options={{
 			attribution:
 				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		}}
 	/> -->
-	<TileLayerWMS
-		baseUrl="http://ows.mundialis.de/services/service?"
-		options={{
-			layers: 'TOPO-OSM-WMS'
-		}}
-	/>
-	<Marker {latlng} />
-	<!-- <Popup options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }}>
+			<TileLayerWMS
+				baseUrl="http://ows.mundialis.de/services/service?"
+				options={{
+					layers: 'TOPO-OSM-WMS'
+				}}
+			/>
+			<Marker {latlng} />
+			<!-- <Popup options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }}>
 		<Marker {latlng} />
 	</Popup> -->
-	<Circle
-		latlng={circleLatlng}
-		options={{
-			color: 'red',
-			fillColor: '#f03',
-			fillOpacity: 0.5,
-			radius: 500
-		}}
-	/>
-	<Polygon
-		latlngs={[
-			[51.509, -0.08],
-			[51.503, -0.06],
-			[51.51, -0.047]
-		]}
-	/>
-	<Polyline
-		latlngs={[
-			[45.51, -122.68],
-			[37.77, -122.43],
-			[34.04, -118.2]
-		]}
-		options={{ color: 'yellow' }}
-	/>
-	<Rectangle
-		latLngBounds={[
-			[54.559322, -5.767822],
-			[56.1210604, -3.02124]
-		]}
-		options={{ color: '#ff7800', weight: 1 }}
-	/>
-	<!-- <Popup {latlng} options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }} /> -->
-	<Marker {latlng}>
-		<Popup options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }} />
-	</Marker>
+			<Circle
+				latlng={circleLatlng}
+				options={{
+					color: 'red',
+					fillColor: '#f03',
+					fillOpacity: 0.5,
+					radius: 500
+				}}
+			/>
+			<Polygon
+				latlngs={[
+					[51.509, -0.08],
+					[51.503, -0.06],
+					[51.51, -0.047]
+				]}
+			/>
+			<Polyline
+				latlngs={[
+					[45.51, -122.68],
+					[37.77, -122.43],
+					[34.04, -118.2]
+				]}
+				options={{ color: 'yellow' }}
+			/>
+			<Rectangle
+				latLngBounds={[
+					[54.559322, -5.767822],
+					[56.1210604, -3.02124]
+				]}
+				options={{ color: '#ff7800', weight: 1 }}
+			/>
+			<!-- <Popup {latlng} options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }} /> -->
+			<Marker {latlng}>
+				<Popup options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }} />
+			</Marker>
 
-	<Marker latlng={[51.509, -0.08]}>
-		<!-- <Popup options={{ content: '234' }} /> -->
-		<Tooltip options={{ content: 'tooltip 234' }} />
-	</Marker>
-	<ControlZoom />
-	<ControlAttribution
-		options={{
-			prefix: 'test attribution'
-		}}
-	/>
-	<ControlScale />
-	<CircleMarker latlng={circleLatlng} options={{ radius: 5 }} />
-</Map>
+			<Marker latlng={[51.509, -0.08]}>
+				<!-- <Popup options={{ content: '234' }} /> -->
+				<Tooltip options={{ content: 'tooltip 234' }} />
+			</Marker>
+			<ControlZoom />
+			<ControlAttribution
+				options={{
+					prefix: 'test attribution'
+				}}
+			/>
+			<ControlScale />
+			<CircleMarker latlng={circleLatlng} options={{ radius: 5 }} />
+		</Map>
+	</div>
+</main>
