e7f58606b553f598a4df3f6b53a5626358e7279e
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-03 11:21:58 +0800

    feat: controls zoom, scale, attribution

---
+++ src/lib/ControlZoom.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { control, type Control } from 'leaflet';
+	import { useConsumeMap } from './context.ts';
+
+  export let options: Control.ZoomOptions = {};
+
+  let mapStore = useConsumeMap();
+  let zoom: Control.Zoom | undefined;
+
+  if ($mapStore) {
+    zoom = control.zoom(options);
+    zoom.addTo($mapStore);
+  }
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,21 @@
 	import Polyline from '$lib/Polyline.svelte';
 	import Rectangle from '$lib/Rectangle.svelte';
 	import Popup from '$lib/Popup.svelte';
+	import ControlZoom from '$lib/ControlZoom.svelte';
+	import ControlAttribution from '$lib/ControlAttribution.svelte';
+	import ControlScale from '$lib/ControlScale.svelte';
 
 	let latlng: LatLngExpression = [51.5, -0.09];
 	let circleLatlng: LatLngExpression = [51.508, -0.11];
 
 	const onMapClick = (e: CustomEvent<LeafletMouseEvent>) => {
 		console.log('on map click', e.detail);
-	}
+	};
 </script>
 
-<Map on:click={onMapClick}>
+<Map on:click={onMapClick} options={{
+	attributionControl: false
+}}>
 	<TileLayer
 		urlTemplate={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
 		options={{
@@ -61,11 +66,16 @@
 		options={{ color: '#ff7800', weight: 1 }}
 	/>
 	<!-- <Popup {latlng} options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }} /> -->
-		<Marker {latlng} >
-			<Popup options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }}/>
-		</Marker>
+	<Marker {latlng}>
+		<Popup options={{ content: '<p>Hello world!<br />This is a nice popup.</p>' }} />
+	</Marker>
 
-		<Marker latlng={[51.509, -0.08]}>
-			<Popup options={{ content: '234' }}/>
-		</Marker>
+	<Marker latlng={[51.509, -0.08]}>
+		<Popup options={{ content: '234' }} />
+	</Marker>
+	<ControlZoom />
+	<ControlAttribution options={{
+		prefix: 'test attribution'
+	}}/>
+	<ControlScale />
 </Map>
