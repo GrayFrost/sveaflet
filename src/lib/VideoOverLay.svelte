94b466eb935e6c2233cfb9a268aeb398adb0763e
zzh <1760655799@qq.com>
2024-06-06 00:02:24 +0800

    feat: overlay

---
+++ src/lib/VideoOverLay.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { useConsumeMap } from './context.ts';
+	import {
+		VideoOverlay,
+		type LatLngBoundsExpression,
+		type VideoOverlayOptions
+	} from 'leaflet?client';
+
+	let mapStore = useConsumeMap();
+
+	export let video: string | string[]; // todoHTMLVideoElement,
+  export let bounds: LatLngBoundsExpression;
+  export let options: VideoOverlayOptions = {};
+
+	if ($mapStore) {
+		let videoOverlay = new VideoOverlay(video, bounds, options);
+		videoOverlay.addTo($mapStore);
+	}
+</script>
+
+<slot />
