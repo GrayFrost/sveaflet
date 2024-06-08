54c99432403837b08f79da354bb522a0ac451ab5
GrayFrost <1760655799@qq.com>
2024-06-08 14:26:40 +0800

    feat: svgoverlay

--- src/lib/SVGOverlay.svelte
+++ src/lib/SVGOverlay.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { SVGOverlay } from 'leaflet';
 	import { useConsumeMap } from './context.ts';
 	import type { LatLngBoundsExpression, ImageOverlayOptions } from 'leaflet';
 
 	let { map: mapStore } = useConsumeMap();
 
-	export let svgImage: string | SVGElement;
+	let svgImage: string | SVGElement;
 	export let bounds: LatLngBoundsExpression;
 	export let options: ImageOverlayOptions = {};
 
-	if ($mapStore) {
-		let svgOverlay = new SVGOverlay(svgImage, bounds, options);
-	}
+	onMount(() => {
+		if ($mapStore) {
+			if (!svgImage) {
+				console.warn('SVG Elements Required!');
+				return;
+			}
+			let svgOverlay = new SVGOverlay(svgImage, bounds, options);
+			svgOverlay.addTo($mapStore);
+		}
+	});
 </script>
 
-<slot />
+<svg bind:this={svgImage} xmlns="http://www.w3.org/2000/svg">
+	<slot />
+</svg>
