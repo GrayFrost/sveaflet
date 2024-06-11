0cf010e97c3a22e8e0f6676f7b6b1f19c65a87c1
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-11 18:32:28 +0800

    wip: controllayers

--- src/lib/TileLayer.svelte
+++ src/lib/TileLayer.svelte
@@ -2,12 +2,14 @@
 	import { onDestroy } from 'svelte';
 	import { TileLayer } from 'leaflet';
 	import type { TileLayerOptions } from 'leaflet';
-	import { useConsumeMap } from './context.ts';
+	import { useConsumeMap, useConsumeControl } from './context.ts';
 
 	export let urlTemplate: string;
-	export let options: TileLayerOptions;
+	export let options: TileLayerOptions = {};
+	export let layerName: string = '';
 
 	let { map: mapStore, removeDefaultLayer } = useConsumeMap();
+	let controlStore = useConsumeControl();
 
 	let tileLayer: TileLayer | undefined;
 
@@ -15,10 +17,18 @@
 		removeDefaultLayer();
 		tileLayer = new TileLayer(urlTemplate, options);
 		tileLayer.addTo($mapStore);
+
+		if ($controlStore) {
+			if (!layerName) {
+				console.warn('Layer Name is required in ControlLayers');
+			} else {
+				$controlStore.addBaseLayer(tileLayer, layerName);
+			}
+		}
 	}
 
 	onDestroy(() => {
 		tileLayer?.remove();
 		tileLayer = undefined;
-	})
+	});
 </script>
