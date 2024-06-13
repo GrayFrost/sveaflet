92fdbad5a304aab16e980a6345055eccb19cafe9
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-13 11:42:12 +0800

    feat: homg page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
-
+	import { Button } from 'flowbite-svelte';
+	import Map from '$lib/Map.svelte';
+	import Marker from '$lib/Marker.svelte';
+	import Popup from '$lib/Popup.svelte';
 </script>
 
-<main
-	class="flex-auto min-w-0 lg:static lg:max-h-full lg:overflow-visible divide-y dark:divide-gray-700"
->
-	<h1>Sveaflet</h1>
-	<h2>Svelte components for Leaflet maps</h2>
+<main class="flex-auto min-w-0 lg:static lg:max-h-full lg:overflow-visible">
+	<p class="flex justify-center my-16">Sveaflet</p>
+	<p class="flex justify-center my-8">Svelte components for Leaflet maps</p>
 
-	<button on:click={() => {console.log('zzh todo')}}>Get Started</button>
+	<div class="flex justify-center my-8">
+		<Button href="/docs/pages/introduction">Get Started</Button>
+	</div>
+
+	<div class="h-[500px] m-8">
+		<Map
+			options={{
+				center: [51.505, -0.09],
+				zoom: 13
+			}}
+		>
+			<Marker latlng={[51.505, -0.09]}>
+				<Popup
+					options={{
+						content: 'A pretty CSS popup.<br> Easily customizable.'
+					}}
+				></Popup>
+			</Marker>
+		</Map>
+	</div>
 </main>
