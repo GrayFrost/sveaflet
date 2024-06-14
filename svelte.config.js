2326bb99441fdbfc4d22f25e83cb9f258c9f3892
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-14 18:01:34 +0800

    chore: add vercel deploy adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
