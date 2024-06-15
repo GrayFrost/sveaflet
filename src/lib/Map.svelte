0b0c2ddd56602bc80dac8f691305e586f4b89b65
zzh <1760655799@qq.com>
2024-06-16 00:09:32 +0800

    feat: fix import error

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
 	import { writable } from 'svelte/store';
-	import { Map, TileLayer } from 'leaflet';
+	import { Map, TileLayer, Icon } from 'leaflet';
 	import type { MapOptions } from 'leaflet';
 	import { useProvideMap } from './context.ts';
 	import 'leaflet/dist/leaflet.css';
+	import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+	import markerIcon from 'leaflet/dist/images/marker-icon.png';
+	import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+	// @ts-ignore
 	delete Icon.Default.prototype._getIconUrl;
   Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
   });
 
 	let mapStore = writable<Map | undefined>();
