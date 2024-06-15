27f21301382bbaae41161eef9ed1cdd850d5b420
zzh <1760655799@qq.com>
2024-06-16 00:04:53 +0800

    fix: marker src 404 in prd env

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -6,6 +6,13 @@
 	import { useProvideMap } from './context.ts';
 	import 'leaflet/dist/leaflet.css';
 
+	delete Icon.Default.prototype._getIconUrl;
+  Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  });
+
 	let mapStore = writable<Map | undefined>();
 	let mapContainer: HTMLElement;
 	let tileLayer: TileLayer | undefined;
