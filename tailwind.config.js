44e6ab2530f3b2e308b5438b0d23d2f9ae9f31e6
GrayFrost <1760655799@qq.com>
2024-06-15 23:29:20 +0800

    feat: revert controllayer style
    After spend more than a hour, finally I fixed it!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,34 @@
-import * as FlowbitePlugin from 'flowbite/plugin';
+// import * as FlowbitePlugin from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-  plugins: [FlowbitePlugin.default],
-  darkMode: 'selector',
-  theme: {
-    extend: {
-      colors: {
-        // flowbite-svelte
-        primary: {
-          50: '#FFF5F2',
-          100: '#FFF1EE',
-          200: '#FFE4DE',
-          300: '#FFD5CC',
-          400: '#FFBCAD',
-          500: '#FE795D',
-          600: '#EF562F',
-          700: '#EB4F27',
-          800: '#CC4522',
-          900: '#A5371B'
-        }
-      },
-      zIndex: {
-        9999: 9999,
-      }
-    }
-  }
-}
-
+	content: [
+		'./src/routes/**/*.{html,js,svelte,ts}',
+    './src/app.html',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+	// plugins: [FlowbitePlugin.default],
+	darkMode: 'selector',
+	theme: {
+		extend: {
+			colors: {
+				// flowbite-svelte
+				primary: {
+					50: '#FFF5F2',
+					100: '#FFF1EE',
+					200: '#FFE4DE',
+					300: '#FFD5CC',
+					400: '#FFBCAD',
+					500: '#FE795D',
+					600: '#EF562F',
+					700: '#EB4F27',
+					800: '#CC4522',
+					900: '#A5371B'
+				}
+			},
+			zIndex: {
+				9999: 9999
+			}
+		}
+	}
+};
