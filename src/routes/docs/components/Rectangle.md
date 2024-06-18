b75bc03f2e02e9b061a85a295fa9a06476eb9f5e
GrayFrost <1760655799@qq.com>
2024-06-18 23:10:12 +0800

    feat: doc

--- src/routes/docs/components/Rectangle.md
+++ src/routes/docs/components/Rectangle.md
@@ -1,13 +1,43 @@
 ---
 layout: componentLayout
 breadcrumb_title: Rectangle
-title: Rectangle
+title: Rectangle - Sveaflet
 component_title: Rectangle
 dir: Components
-description: Rectangle
+description: Vector Layers - Rectangle
 ---
 
-# Rectangle
+A component for drawing rectangle overlays on a map. Extends [Polygon](https://leafletjs.com/reference.html#polygon).
+
+## Setup
+
+```svelte example csr hideOutput
+<script>
+	import { Rectangle } from 'sveaflet';
+</script>
+```
+
+## Default Rectangle
+
+```svelte example csr
+<script>
+	import { Map, TileLayer, Rectangle } from 'sveaflet';
+</script>
+
+<div style="width: 100%;height: 500px;">
+	<Map options={{ center: [55, -4.5], zoom: 6 }}>
+		<TileLayer urlTemplate={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+		<Rectangle
+			latLngBounds={[
+				[54.559322, -5.767822],
+				[56.1210604, -3.02124]
+			]}
+		/>
+	</Map>
+</div>
+```
+
+## Rectangle with Options
 
 ```svelte example csr
 <script>
@@ -28,4 +58,32 @@ description: Rectangle
 </div>
 ```
 
-// todo doc
+## Rectangle with Popup
+
+```svelte example csr
+<script>
+	import { Map, TileLayer, Rectangle, Popup } from 'sveaflet';
+</script>
+
+<div style="width: 100%;height: 500px;">
+	<Map options={{ center: [55, -4.5], zoom: 6 }}>
+		<TileLayer urlTemplate={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+		<Rectangle
+			latLngBounds={[
+				[54.559322, -5.767822],
+				[56.1210604, -3.02124]
+			]}
+			options={{ color: '#ff7800' }}
+		>
+			<Popup options={{ content: 'Popup in Rectangle.' }} />
+		</Rectangle>
+	</Map>
+</div>
+```
+
+## Props
+
+| Prop name    | Description  | Type                                                                    | Default |
+| ------------ | ------------ | ----------------------------------------------------------------------- | ------- |
+| latLngBounds | **Required** | [LatLngBounds](https://leafletjs.com/reference.html#latlngbounds)       |         |
+| options      | **Optional** | [PolylineOptions](https://leafletjs.com/reference.html#polyline-option) | `{}`    |
--- src/routes/utils/Footer.svelte
+++ src/routes/utils/Footer.svelte
@@ -1,54 +1,65 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import { Footer, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+	import { Footer, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
 
-  let isHomePage: boolean;
-  $: isHomePage = $page.route.id === '/';
+	let isHomePage: boolean;
+	$: isHomePage = $page.route.id === '/';
 
-  const footer_links = {
-    Resources: {
-      GitHub: 'todo',
-      Flowbite: 'todo',
-      'Tailwind CSS': 'https://tailwindcss.com/',
-      'todo': 'todo'
-    },
-    'Help and Support': {
-      'Discord Community': 'todo',
-      'GitHub Discussions': 'todo'
-    },
-    Legal: {
-      License: 'todo',
-      'Brand usage': 'todo'
-    }
-  };
+	const footer_links = {
+		Resources: {
+			GitHub: 'https://github.com/GrayFrost/sveaflet',
+			Svelte: 'https://svelte.dev/',
+			Leaflet: 'https://leafletjs.com/',
+			'Flowbite Svelte': 'https://flowbite-svelte.com/'
+		},
+		'Help and Support': {
+			'GitHub Issues': 'https://github.com/GrayFrost/sveaflet/issues'
+		},
+		Legal: {
+			License: 'todo',
+			'Brand usage': 'todo'
+		}
+	};
 </script>
 
 <Footer class="bg-white dark:bg-gray-900">
-  <div class="flex flex-col py-6 lg:py-12 mx-auto max-w-8xl">
-    <div class="flex flex-col lg:flex-row items-start md:justify-between gap-8 {isHomePage ? 'px-4 lg:px-20' : ''}">
-      <div class="w-full max-w-sm">
-        <!-- <FooterBrand href="https://flowbite-svelte.com" src={logo} alt="Flowbite Svelte Logo" name="Flowbite Svelte" class="text-gray-900 dark:text-white" /> -->
-        <p class="max-w-sm mb-3 text-gray-600 dark:text-gray-400 mt-4">Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte</p>
-        <p class="max-w-sm mb-3 text-gray-600 dark:text-gray-400">
-          Code licensed
-          <a href="todo" class="text-primary-600 hover:underline"> todo </a>
-          , docs
-          <a href="todo" class="text-primary-600 hover:underline"> todo</a>
-          .
-        </p>
-      </div>
-      <div class="flex flex-col md:flex-row items-start {isHomePage ? 'gap-4 md:gap-16 lg:justify-end' : 'gap-8'}  w-full">
-        {#each Object.entries(footer_links) as [column, links]}
-          <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">{column}</h2>
-            <FooterLinkGroup>
-              {#each Object.entries(links) as [name, href]}
-                <FooterLink liClass="mb-4" {href}>{name}</FooterLink>
-              {/each}
-            </FooterLinkGroup>
-          </div>
-        {/each}
-      </div>
-    </div>
-  </div>
+	<div class="flex flex-col py-6 lg:py-12 mx-auto max-w-8xl">
+		<div
+			class="flex flex-col lg:flex-row items-start md:justify-between gap-8 {isHomePage
+				? 'px-4 lg:px-20'
+				: ''}"
+		>
+			<div class="w-full max-w-sm">
+				<!-- <FooterBrand href="https://flowbite-svelte.com" src={logo} alt="Flowbite Svelte Logo" name="Flowbite Svelte" class="text-gray-900 dark:text-white" /> -->
+				<p class="max-w-sm mb-3 text-gray-600 dark:text-gray-400 mt-4">
+					Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte Svelte
+				</p>
+				<p class="max-w-sm mb-3 text-gray-600 dark:text-gray-400">
+					Code licensed
+					<a href="todo" class="text-primary-600 hover:underline"> todo </a>
+					, docs
+					<a href="todo" class="text-primary-600 hover:underline"> todo</a>
+					.
+				</p>
+			</div>
+			<div
+				class="flex flex-col md:flex-row items-start {isHomePage
+					? 'gap-4 md:gap-16 lg:justify-end'
+					: 'gap-8'}  w-full"
+			>
+				{#each Object.entries(footer_links) as [column, links]}
+					<div>
+						<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+							{column}
+						</h2>
+						<FooterLinkGroup>
+							{#each Object.entries(links) as [name, href]}
+								<FooterLink liClass="mb-4" {href}>{name}</FooterLink>
+							{/each}
+						</FooterLinkGroup>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</div>
 </Footer>
