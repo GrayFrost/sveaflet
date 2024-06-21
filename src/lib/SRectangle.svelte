a309aab3484b0142cf67f9683b5b9385c4a3ad17
GrayFrost <1760655799@qq.com>
2024-06-21 22:27:07 +0800

    feat: rectangle

--- src/lib/SRectangle.svelte
+++ src/lib/SRectangle.svelte
@@ -1,30 +1,90 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { Rectangle } from 'leaflet';
-	import type { LatLngBoundsExpression, PolylineOptions } from 'leaflet';
+	import type { LatLngBoundsExpression, PathOptions, PolylineOptions } from 'leaflet';
 	import { useConsumeMap, useConsumeLayerGroup, useProvideLayer } from '$lib/context';
 
+	// props
 	export let latLngBounds: LatLngBoundsExpression;
 	export let options: PolylineOptions = {};
 	export let instance: Rectangle | undefined = undefined;
 
+	// store
 	let mapStore = useConsumeMap();
 	let layerGroupStore = useConsumeLayerGroup();
 	let rectangleStore = writable<Rectangle | undefined>();
 
-	$: if ($mapStore) {
-		reset();
-		$rectangleStore = new Rectangle(latLngBounds, options);
+	// data
+	let preLatLngBounds = latLngBounds;
+	let preOptions = options;
 
-		if ($layerGroupStore) {
-			$layerGroupStore.addLayer($rectangleStore);
-		} else {
-			$rectangleStore.addTo($mapStore);
+	onMount(() => {
+		$rectangleStore = new Rectangle(latLngBounds, options);
+		storeProps({ latLngBounds, options });
+	});
+
+	$: if ($mapStore) {
+		if ($rectangleStore) {
+			updateBounds($rectangleStore, preLatLngBounds, latLngBounds);
+			updateStyle($rectangleStore, preOptions, options);
+
+			if ($layerGroupStore) {
+				$layerGroupStore.addLayer($rectangleStore);
+			} else {
+				$rectangleStore.addTo($mapStore);
+			}
+			storeProps({
+				latLngBounds,
+				options
+			});
 		}
 	}
 
 	$: instance = $rectangleStore;
+
+	function updateBounds(
+		obj: Rectangle,
+		preLatLngBounds: LatLngBoundsExpression,
+		latLngBounds: LatLngBoundsExpression
+	) {
+		if (latLngBounds !== preLatLngBounds && latLngBounds !== undefined) {
+			obj.setBounds(latLngBounds);
+		}
+	}
+
+	function updateStyle(obj: Rectangle, preOpt: PolylineOptions, opt: PolylineOptions) {
+		const keys: Array<keyof PathOptions> = [
+			'stroke',
+			'color',
+			'weight',
+			'opacity',
+			'lineCap',
+			'lineJoin',
+			'dashArray',
+			'dashOffset',
+			'fill',
+			'fillColor',
+			'fillOpacity',
+			'fillRule',
+			'renderer',
+			'className'
+		];
+
+		const styles: Partial<Record<keyof PathOptions, any>> = {};
+		keys.forEach((key) => {
+			if (opt[key] !== preOpt[key] && opt[key] !== undefined) {
+				styles[key] = opt[key];
+			}
+		});
+		obj.setStyle(styles);
+	}
+
+	function storeProps(props: { latLngBounds: LatLngBoundsExpression; options: PolylineOptions }) {
+		const { latLngBounds, options } = props;
+		preLatLngBounds = latLngBounds;
+		preOptions = Object.create(options);
+	}
 
 	function reset() {
 		$rectangleStore?.remove();
