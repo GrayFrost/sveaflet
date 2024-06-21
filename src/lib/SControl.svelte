e56e856367cb4448374c8e841bc82b4fc6801119
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-21 15:52:56 +0800

    feat: control

--- src/lib/SControl.svelte
+++ src/lib/SControl.svelte
@@ -4,14 +4,19 @@
 	import type { ControlOptions } from 'leaflet';
 	import { useConsumeMap } from '$lib/context';
 
+	// props
 	export let options: ControlOptions = { position: 'topright' };
 	export let instance: Control | undefined = undefined;
 
+	// store
 	let mapStore = useConsumeMap();
 
+	// data
 	let control: Control | undefined;
 	let htmlElement: HTMLElement;
+	let preOptions = options;
 
+	// class
 	let CustomControl: { new (...args: any[]): { onAdd(): HTMLElement } } & typeof Control;
 
 	onMount(() => {
@@ -21,15 +26,28 @@
 				return htmlElement;
 			}
 		});
+		control = new CustomControl(options);
+		storeProps();
 	});
 
-	$: if ($mapStore && CustomControl) {
-		reset();
-		control = new CustomControl(options);
-		control.addTo($mapStore);
+	$: if ($mapStore) {
+		if (control) {
+			updatePosition(control, preOptions, options);
+			control.addTo($mapStore);
+		}
 	}
 
 	$: instance = control;
+
+	function updatePosition(obj: Control, preOpt: ControlOptions, opt: ControlOptions) {
+		if (opt.position !== preOpt.position && opt.position !== undefined) {
+			obj.setPosition(opt.position);
+		}
+	}
+
+	function storeProps() {
+		preOptions = Object.create(options);
+	}
 
 	function reset() {
 		control?.remove();
