b4b2b0683805b3d01a8b6c59fb4681a13da38b87
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-21 16:46:58 +0800

    feat: geojson

--- src/lib/SGeoJSON.svelte
+++ src/lib/SGeoJSON.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { GeoJSON } from 'leaflet';
 	import type { GeoJSONOptions } from 'leaflet';
 	import type { GeoJsonObject } from 'geojson';
 	import { useConsumeMap, useProvideLayer } from '$lib/context';
 
+	// props
 	export let json: GeoJsonObject | null = null;
 	export let options: GeoJSONOptions | null = null;
-	export let instance: GeoJSON | undefined;
+	export let instance: GeoJSON | undefined = undefined;
 
+	// store
 	let mapStore = useConsumeMap();
 	let geoJSONStore = writable<GeoJSON | undefined>();
 
-	$: if ($mapStore) {
-		reset();
+	onMount(() => {
 		$geoJSONStore = new GeoJSON(json, options);
-		$geoJSONStore.addTo($mapStore);
+	});
+
+	$: if ($mapStore) {
+		if ($geoJSONStore) {
+			$geoJSONStore.addTo($mapStore);
+		}
 	}
 
 	$: instance = $geoJSONStore;
