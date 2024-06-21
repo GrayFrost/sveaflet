e431304113c8720e3f8657ea17f22047a400b5ba
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-21 16:19:54 +0800

    feat: controlscale

--- src/lib/SControlScale.svelte
+++ src/lib/SControlScale.svelte
@@ -1,23 +1,48 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { control } from 'leaflet';
 	import type { Control } from 'leaflet';
 	import { useConsumeMap } from '$lib/context';
 
+	// props
 	export let options: Control.ScaleOptions = {};
 	export let instance: Control.Scale | undefined = undefined;
 
+	// store
 	let mapStore = useConsumeMap();
 
+	// data
 	let scale: Control.Scale | undefined;
+	let preOptions = options;
+
+	onMount(() => {
+		scale = control.scale(options);
+		storeProps();
+	});
 
 	$: if ($mapStore) {
-		reset();
-		scale = control.scale(options);
-		scale.addTo($mapStore);
+		if (scale) {
+			updatePosition(scale, preOptions, options);
+			scale.addTo($mapStore);
+			storeProps();
+		}
 	}
 
 	$: instance = scale;
+
+	function updatePosition(
+		obj: Control.Scale,
+		preOpt: Control.ScaleOptions,
+		opt: Control.ScaleOptions
+	) {
+		if (opt.position !== preOpt.position && opt.position !== undefined) {
+			obj.setPosition(opt.position);
+		}
+	}
+
+	function storeProps() {
+		preOptions = Object.create(options);
+	}
 
 	function reset() {
 		scale?.remove();
