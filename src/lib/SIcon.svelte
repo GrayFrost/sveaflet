5728ba796ecf83dd69aa1b156125f5f43637859c
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-21 16:52:33 +0800

    feat: icon

--- src/lib/SIcon.svelte
+++ src/lib/SIcon.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import { Icon } from 'leaflet';
 	import type { IconOptions } from 'leaflet';
 	import { useConsumeMap, useConsumeMarker } from '$lib/context';
 
+	// props
 	export let options: IconOptions = { iconUrl: '' };
 	export let instance: Icon | undefined = undefined;
 
+	// store
 	let mapStore = useConsumeMap();
 	let markerStore = useConsumeMarker();
 
+	// data
 	let icon: Icon | undefined;
 
+	onMount(() => {
+		icon = new Icon(options);
+	});
+
 	$: if ($mapStore) {
-		if ($markerStore) {
-			reset();
-			icon = new Icon(options);
-			$markerStore.setIcon(icon);
-		} else {
-			console.warn('Icon should bind Marker.');
+		if (icon) {
+			if ($markerStore) {
+				$markerStore.setIcon(icon);
+			} else {
+				console.warn('Icon should bind Marker.');
+			}
 		}
 	}
 
