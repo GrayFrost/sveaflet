b658481b22eeed906d0162210d0d42acf1d7579d
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-21 18:13:11 +0800

    feat: tilelayer

--- src/lib/STileLayer.svelte
+++ src/lib/STileLayer.svelte
@@ -4,37 +4,38 @@
 	import type { TileLayerOptions } from 'leaflet';
 	import { useConsumeMap, useConsumeControlLayer } from '$lib/context';
 
+	// props
 	export let urlTemplate: string;
 	export let options: TileLayerOptions = {};
 	export let layerName: string = '';
 	export let checked: boolean = false;
 	export let instance: TileLayer | undefined = undefined;
 
+	// store
 	let mapStore = useConsumeMap();
 	let controlLayerStore = useConsumeControlLayer();
 
+	// data
 	let tileLayer: TileLayer | undefined;
-
 	let preUrlTemplate = urlTemplate;
 	let preOptions = options;
 
 	onMount(() => {
 		tileLayer = new TileLayer(urlTemplate, options);
-		storeProps();
+		storeProps({
+			urlTemplate,
+			options
+		});
 	});
 
 	$: if ($mapStore) {
 		if (tileLayer) {
 			// TODO: how to update all options?
+			updateUrl(tileLayer, preUrlTemplate, urlTemplate);
+			updateOpacity(tileLayer, preOptions, options);
+			updateZIndex(tileLayer, preOptions, options);
 			if (urlTemplate !== preUrlTemplate && urlTemplate !== undefined) {
 				tileLayer.setUrl(urlTemplate);
-			}
-			if (options.zIndex !== preOptions.zIndex && options.zIndex !== undefined) {
-				tileLayer.setZIndex(options.zIndex);
-			}
-
-			if (options.opacity !== preOptions.opacity && options.opacity !== undefined) {
-				tileLayer.setOpacity(options.opacity);
 			}
 
 			if ($controlLayerStore) {
@@ -52,13 +53,35 @@
 				tileLayer.addTo($mapStore);
 			}
 
-			storeProps();
+			storeProps({
+				urlTemplate,
+				options
+			});
 		}
 	}
 
 	$: instance = tileLayer;
 
-	function storeProps() {
+	function updateUrl(obj: TileLayer, preBaseUrl: string, baseUrl: string) {
+		if (baseUrl !== preBaseUrl && baseUrl !== undefined) {
+			obj.setUrl(baseUrl);
+		}
+	}
+
+	function updateOpacity(obj: TileLayer, preOpt: TileLayerOptions, opt: TileLayerOptions) {
+		if (opt.opacity !== preOpt.opacity && opt.opacity !== undefined) {
+			obj.setOpacity(opt.opacity);
+		}
+	}
+
+	function updateZIndex(obj: TileLayer, preOpt: TileLayerOptions, opt: TileLayerOptions) {
+		if (opt.zIndex !== preOpt.zIndex && opt.zIndex !== undefined) {
+			obj.setZIndex(opt.zIndex);
+		}
+	}
+
+	function storeProps(props: { urlTemplate: string; options: TileLayerOptions }) {
+		const { urlTemplate, options } = props;
 		preUrlTemplate = urlTemplate;
 		preOptions = Object.create(options);
 	}
@@ -66,7 +89,6 @@
 	function reset() {
 		tileLayer?.remove();
 		tileLayer = undefined;
-		// todo other props
 	}
 
 	onDestroy(() => {
