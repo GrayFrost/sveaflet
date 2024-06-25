65fb79022a5465f14b455833e00af357548af127
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-06-25 11:27:18 +0800

    feat: code expand

--- src/routes/utils/ExampleWrapper.svelte
+++ src/routes/utils/ExampleWrapper.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { twJoin, twMerge } from 'tailwind-merge';
-	import { onMount } from 'svelte';
+	import { twMerge } from 'tailwind-merge';
 	import { Tooltip } from 'flowbite-svelte';
+	import { AngleDownOutline, AngleUpOutline } from 'flowbite-svelte-icons';
 	export let divClass = 'w-full mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 p-6';
 	// the source of the example, if you want it
 	export let src: any = undefined;
@@ -52,15 +52,14 @@
 		showExpandButton = isOverflowingY;
 		el.firstElementChild?.classList.add('-mb-8');
 	}
-
-
 </script>
 
 <div class="mt-8 code-example" bind:this={node} use:init>
 	{#if !meta.hideOutput}
 		<div class="code-preview-wrapper">
 			<div
-				class="flex p-0 bg-white border-gray-200 bg-gradient-to-r code-preview dark:bg-gray-900 border-x border-t dark:border-gray-600" class:dark
+				class="flex p-0 bg-white border-gray-200 bg-gradient-to-r code-preview dark:bg-gray-900 border-x border-t dark:border-gray-600"
+				class:dark
 			>
 				<div class="w-full code-responsive-wrapper">
 					<div class={twMerge(divClass, meta.class)}>
@@ -119,14 +118,18 @@
 							/></pre>
 					</div>
 				</div>
-				{#if showExpandButton && !expand}
+				{#if showExpandButton}
 					<button
 						on:click={() => (expand = !expand)}
 						data-expand-code=""
 						type="button"
-						class="absolute bottom-0 start-0 py-2.5 px-5 w-full text-sm font-medium text-gray-900 bg-gray-100 border-t border-gray-200 hover:bg-gray-100 hover:text-primary-700 dark:bg-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+						class="flex items-center justify-center absolute bottom-0 start-0 py-2.5 px-5 w-full text-sm font-medium text-gray-900 bg-gray-100 border-t border-gray-200 hover:bg-gray-100 hover:text-primary-700 dark:bg-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
 					>
-						Expand code
+						{#if !expand}
+							Expand code <AngleDownOutline />
+						{:else}
+							Collapse code<AngleUpOutline />
+						{/if}
 					</button>
 				{/if}
 			</div>
