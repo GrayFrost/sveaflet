e80cf8cac17c0528ed280c7ce6004bc0345dc87f
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-08-01 18:03:52 +0800

    feat: add algoliasearch

---
+++ src/routes/utils/AlgoliaSearch.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
+  import docsearch from '@docsearch/js';
+  import '@docsearch/css';
+
+	export let appId: string;
+	export let apiKey: string;
+
+  let searchContainer: HTMLElement;
+
+  onMount(() => {
+    if (browser) {
+      // Algolia docsearch
+      docsearch({
+        container: searchContainer,
+        appId,
+        indexName: 'sveaflet',
+        apiKey,
+        placeholder: 'Search documentation'
+      });
+    }
+  });
+</script>
+
+<div bind:this={searchContainer} id="docsearch" class="ms-3 xl:ps-4" />
