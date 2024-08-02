e154eeefd68c505567eb7bedf05b370c59b6aac3
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-08-02 18:00:46 +0800

    wip: unit test

---
+++ src/lib/greeter.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name
+
+  let showGreeting = false
+
+  const handleClick = () => (showGreeting = true)
+</script>
+
+<button on:click="{handleClick}">Greet</button>
+
+{#if showGreeting}
+<p>Hello {name}</p>
+{/if}
