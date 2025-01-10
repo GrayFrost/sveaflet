d9c1c8162a23dd7bb48342df996faa861a11b685
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-01-10 14:14:54 +0800

    feat: tilelayer migrate

--- src/lib/STileLayer.svelte
+++ src/lib/STileLayer.svelte
@@ -1,62 +1,95 @@
-<!-- @migration-task Error while migrating Svelte code: $$props is used together with named props in a way that cannot be automatically migrated. -->
 <script lang="ts">
 	import { onMount, onDestroy, getContext } from 'svelte';
 	import { Map, TileLayer } from 'leaflet';
-	import type { TileLayerOptions } from 'leaflet';
+	import type { TileLayerOptions, Control } from 'leaflet';
 	import type { LeafletContextInterface } from './types';
 	import { setControlLayer, Compare } from './utils/index';
 
 	// props
-	export let url: string;
-	export let options: TileLayerOptions = {};
-	export let name: string = '';
-	export let checked: boolean = false;
-	export let instance: TileLayer | undefined = undefined;
-	export let layerType: 'base' | 'overlay' | undefined = undefined;
+	type Props = {
+		url: string;
+		options: TileLayerOptions;
+		name: string;
+		checked: boolean;
+		instance: TileLayer | undefined;
+		layerType: 'base' | 'overlay' | undefined;
+	};
+
+	let {
+		url,
+		options = {},
+		name = '',
+		checked = false,
+		instance = $bindable(),
+		layerType
+	}: Props = $props();
 
 	// context
 	let parentContext = getContext<LeafletContextInterface>(Map);
 	const { getMap, getControl } = parentContext;
 
 	// data
-	let tileLayer: TileLayer | undefined;
-	let compare: Compare;
+	let tileLayer: TileLayer | undefined = $state();
+	let map: Map | undefined = $state();
+	let controlLayers: Control.Layers | undefined = $state();
+	let compare: Compare | undefined = $state.raw();
 
-	$: map = getMap?.();
-	$: controlLayers = getControl?.();
-	$: instance = tileLayer;
-
-	onMount(() => {
-		tileLayer = new TileLayer(url, options);
-		compare = new Compare(tileLayer, $$props);
+	$effect(() => {
+		map = getMap?.();
+		controlLayers = getControl?.();
 	});
 
-	$: if (map) {
-		if (tileLayer) {
-			compare.updateProps($$props);
+	$effect(() => {
+		instance = tileLayer;
+	});
 
-			if (controlLayers) {
-				if (!name) {
-					console.warn('Name is required in ControlLayers');
+	onMount(() => {
+		const props = {
+			url,
+			options,
+			name,
+			checked,
+			layerType
+		};
+		tileLayer = new TileLayer(url, options);
+		compare = new Compare(tileLayer, props);
+	});
+
+	$effect(() => {
+		if (map) {
+			if (tileLayer) {
+				const props = {
+					url,
+					options,
+					name,
+					checked,
+					layerType
+				};
+				compare?.updateProps(props);
+
+				if (controlLayers) {
+					if (!name) {
+						console.warn('Name is required in ControlLayers');
+					} else {
+						setControlLayer({
+							layer: tileLayer,
+							name,
+							controlLayers,
+							layerType,
+							map,
+							checked
+						});
+					}
+
+					controlLayers.addTo(map);
 				} else {
-					setControlLayer({
-						layer: tileLayer,
-						name,
-						controlLayers,
-						layerType,
-						map,
-						checked
-					});
+					map.addLayer(tileLayer);
 				}
 
-				controlLayers.addTo(map);
-			} else {
-				map.addLayer(tileLayer);
+				compare?.storeProps(props);
 			}
-
-			compare.storeProps($$props);
 		}
-	}
+	});
 
 	function reset() {
 		tileLayer?.remove();
